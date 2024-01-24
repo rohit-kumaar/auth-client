c24dc8ee0e6283e3acb4a2612183169ef5daeab3
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2024-01-24 17:31:56 +0530

    authenticate middleware get token

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+
+const url = "http://localhost:4000/api/v1";
 
 function Dashboard() {
+  const token = localStorage.getItem("userDataToken");
+
+  const dashboardValid = async () => {
+    axios({
+      method: "get",
+      baseURL: `${url}/valid-user`,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    })
+      .then((res) => {
+        const data = res.data;
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    dashboardValid();
+  }, []);
+
   return <div>Dashboard</div>;
 }
 
