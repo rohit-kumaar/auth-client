6caf03cf232f25d6cc529644e116e199c06275fc
Rohit Kumar <rohit96682@gmail.com>
2024-01-27 14:49:30 +0530

    token is true  then jump to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,56 @@
-import React from "react";
-
+import axios from "axios";
+import React, { lazy, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import "./App.scss";
+import { setUserData } from "./features/userSlice.js";
+import { ROUTE_PATH } from "./routes/path.js";
+const Dashboard = lazy(() => import("./pages/Dashboard.jsx"));
+const Error = lazy(() => import("./pages/Error.jsx"));
+const Login = lazy(() => import("./pages/Login.jsx"));
+const SignUp = lazy(() => import("./pages/SignUp.jsx"));
 
 function App() {
-  return <div className="bg-dark text-white fs-1">App</div>;
+  const token = localStorage.getItem("userDataToken");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const url = "http://localhost:4000/api/v1";
+
+  const dashboardValid = async () => {
+    axios
+      .get(`${url}/valid-user`, {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((res) => {
+        const data = res.data;
+        console.log(data);
+
+        if (data.status == 401 || !data) {
+          navigate(ROUTE_PATH.ERROR);
+        } else {
+          dispatch(setUserData(data));
+          navigate(ROUTE_PATH.DASHBOARD);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    token ? dashboardValid() : null;
+  }, []);
+
+  return (
+    <Routes>
+      <Route path={ROUTE_PATH.DEFAULT} element={<SignUp />} />
+      <Route path={ROUTE_PATH.LOGIN} element={<Login />} />
+      <Route path={ROUTE_PATH.DASHBOARD} element={<Dashboard />} />
+      <Route path={ROUTE_PATH.ERROR} element={<Error />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,17 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import "./App.scss";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 import store from "./app/store.js";
-import { ROUTE_PATH } from "./routes/path.js";
-const Dashboard = lazy(() => import("./pages/Dashboard.jsx"));
-const Error = lazy(() => import("./pages/Error.jsx"));
-const Login = lazy(() => import("./pages/Login.jsx"));
-const SignUp = lazy(() => import("./pages/SignUp.jsx"));
-
-const router = createBrowserRouter([
-  {
-    path: ROUTE_PATH.DEFAULT,
-    element: <SignUp />,
-  },
-  {
-    path: ROUTE_PATH.LOGIN,
-    element: <Login />,
-  },
-  {
-    path: ROUTE_PATH.DASHBOARD,
-    element: <Dashboard />,
-  },
-  {
-    path: ROUTE_PATH.ERROR,
-    element: <Error />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <React.StrictMode>
       <Suspense fallback="Loading...">
-        <RouterProvider router={router} />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </Suspense>
     </React.StrictMode>
   </Provider>
